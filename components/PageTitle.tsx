32e0245949eada24ea16966a31323882c28dde38
Ryan Fitton <ryan@ryanfitton.co.uk>
2024-10-21 15:14:49 +0100

    Revert as React.createElement is deprecated

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -8,7 +8,11 @@ interface Props {
 }
 //https://stackoverflow.com/questions/33471880/dynamic-tag-name-in-react-jsx
 export default function PageTitle({ className, headingSize, heading }: Props) {
-  const CustomTag = `${headingSize ? headingSize : 'h1'}`
+  const CustomTag = `${headingSize ? headingSize : 'h1'}` as keyof JSX.IntrinsicElements
 
-  return createElement(CustomTag, { className: className }, heading)
+  return (
+    <CustomTag className={`hero__title component-title ` + (className ? className : '')}>
+      {heading}
+    </CustomTag>
+  )
 }
