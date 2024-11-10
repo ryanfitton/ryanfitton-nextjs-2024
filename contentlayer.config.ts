1f57f70771ec2439b229e3b296f84a665b566045
Ryan Fitton <ryan@ryanfitton.co.uk>
2024-11-10 20:48:51 +0000

    Updates to blogs and remove katex maths rehype plugin

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -19,7 +19,6 @@ import {
 // Rehype packages
 import rehypeSlug from 'rehype-slug'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypeKatex from 'rehype-katex'
 import rehypeCitation from 'rehype-citation'
 import rehypePrismPlus from 'rehype-prism-plus'
 import rehypePresetMinify from 'rehype-preset-minify'
