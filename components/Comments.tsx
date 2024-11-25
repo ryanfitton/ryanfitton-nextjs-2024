3533387a1ffc8021dbf550c3a9a58ee45b6a4c0a
Ryan Fitton <ryan@ryanfitton.co.uk>
2024-11-25 16:54:21 +0000

    Add JS for colour scheme swticher on disqus

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -2,12 +2,38 @@
 
 import { Comments as CommentsComponent } from 'pliny/comments'
 import siteMetadata from '@/data/siteMetadata'
+import Script from 'next/script'
 
 export default function Comments({ slug }: { slug: string }) {
   return (
     <>
       {siteMetadata.comments && (
-        <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
+        <>
+          <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
+
+          <Script
+            dangerouslySetInnerHTML={{
+              __html: `
+                if (typeof window !== 'undefined' && window.matchMedia) {
+                  const iframe = document.getElementById('disqus_thread').querySelector('iframe');
+
+                  if (iframe && iframe.length() > 0) {
+
+                    const media = window.matchMedia('(prefers-color-scheme: dark)');
+                    media.addEventListener('change', event => {
+                      colorScheme = event.matches ? "dark" : "light";
+
+                      iframe.contentDocument.getElementByTagName('html').style["color-scheme"] = colorScheme
+
+                    });
+
+                  }
+                }
+              `
+            }}
+          />
+          
+        </>
       )}
     </>
   )
