d1b2ba97504e3e5c925bb74f58d04aa3cbe7666a
Ryan Fitton <ryanfitton@users.noreply.github.com>
2024-11-26 13:11:12 +0000

    Update rss.mjs

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -7,11 +7,7 @@ import tagData from '../app/tag-data.json' assert { type: 'json' }
 import { allBlogs } from '../.contentlayer/generated/index.mjs'
 import { sortPosts } from 'pliny/utils/contentlayer.js'
 
-const cwd = process.cwd()
-
-const outputFolder = process.env.EXPORT ? 'out' : 'public'
-
-console.log(outputFolder)
+const outputFolder = process.env.EXPORT ? 'build' : 'public'
 
 const generateRssItem = (config, post) => `
   <item>
@@ -42,69 +38,34 @@ const generateRss = (config, posts, page = 'feed.xml') => `
 `
 
 async function generateRSS(config, allBlogs, page = 'feed.xml') {
-
-
-  // Writing to the file 5 times
-  // with the append file mode
-  for (let i = 0; i < 5; i++) {
-    writeFileSync(path.resolve(`${cwd}`, `./${outputFolder}/static/movies.txt`), 'Movie ' + i + '\n', {
-      encoding: 'utf8',
-      flag: 'a+',
-      mode: 0o666,
-    })
-  }
-
-  console.log('File written successfully 5 times\n')
-  console.log('The written file has the following contents:')
-  console.log(readFileSync(path.resolve(`${cwd}`, `./${outputFolder}/static/movies.txt`), 'utf8'))
-
-
-
-
   const publishPosts = allBlogs.filter((post) => post.draft !== true)
   // RSS for blog post
   if (publishPosts.length > 0) {
-    //console.log('publishPosts.length > 0  -- TRUE')
-
     const rss = generateRss(config, sortPosts(publishPosts))
 
-    //console.log(rss)
-
     try {
-      //console.log('Trying to write rss ' + `./${outputFolder}/${page}`)
       writeFileSync(`./${outputFolder}/${page}`, rss)
     } catch (err) {
       console.log('Error writing ' + `./${outputFolder}/${page}`)
-      //console.log(rss)
       throw err
     }
 
     for (const tag of Object.keys(tagData)) {
-      //console.log('const tag of Object.keys(tagData) -- ' + tag)
-
       const filteredPosts = allBlogs.filter((post) => post.tags.map((t) => slug(t)).includes(tag))
       const rss = generateRss(config, filteredPosts, `tags/${tag}/${page}`)
-
-      //console.log(rss)
       const rssPath = path.join(outputFolder, 'tags', tag)
-      //console.log(rssPath)
 
       try {
-        //console.log('Trying to make directory rssPath ' + `${rssPath}`)
         mkdirSync(rssPath, { recursive: true })
       } catch (err) {
         console.log('Error trying to make directory rssPath ' + `${rssPath}`)
-        console.log(rssPath)
         throw err
       }
 
       try {
-        //console.log('Trying to write rss ' + `${path.join(rssPath, page)}`)
         writeFileSync(path.join(rssPath, page), rss)
       } catch (err) {
         console.log('Error writing ' + `${path.join(rssPath, page)}`)
-        //console.log(path.join(rssPath, page))
-        //console.log(rss)
         throw err
       }
     }
@@ -113,6 +74,5 @@ async function generateRSS(config, allBlogs, page = 'feed.xml') {
 
 const rss = () => {
   generateRSS(siteMetadata, allBlogs)
-  //console.log('RSS feed generated...')
 }
 export default rss
