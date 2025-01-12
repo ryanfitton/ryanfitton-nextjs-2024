fc58dddf2fe6e9e68471ea1fb42e89344acb79d0
Ryan Fitton <ryan@ryanfitton.co.uk>
2025-01-12 15:36:57 +0000

    Fixes for heading icon

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -34,7 +34,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 //Heroicon mini link (https://heroicons.com/mini)
 const rehypeAutolinkHeadingsIcon = fromHtmlIsomorphic(
   `
-  <span className="content-header-link">
+  <span className="component-posts-article__title-icon">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 linkicon">
         <path d="M12.232 4.232a2.5 2.5 0 0 1 3.536 3.536l-1.225 1.224a.75.75 0 0 0 1.061 1.06l1.224-1.224a4 4 0 0 0-5.656-5.656l-3 3a4 4 0 0 0 .225 5.865.75.75 0 0 0 .977-1.138 2.5 2.5 0 0 1-.142-3.667l3-3Z" />
         <path d="M11.603 7.963a.75.75 0 0 0-.977 1.138 2.5 2.5 0 0 1 .142 3.667l-3 3a2.5 2.5 0 0 1-3.536-3.536l1.225-1.224a.75.75 0 0 0-1.061-1.06l-1.224 1.224a4 4 0 1 0 5.656 5.656l3-3a4 4 0 0 0-.225-5.865Z" />
@@ -226,7 +226,7 @@ export default makeSource({
         {
           behavior: 'prepend',
           headingProperties: {
-            className: ['content-header'],
+            className: ['component-posts-article__title'],
           },
           content: rehypeAutolinkHeadingsIcon,
         },
