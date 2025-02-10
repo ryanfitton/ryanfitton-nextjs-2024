7fab528d9022e9bd3ca27d903b611a243408a274
Ryan Fitton <ryan@ryanfitton.co.uk>
2025-02-10 12:07:02 +0000

    Initial updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   plugins: {
-    'postcss-preset-env': {},
-    "postcss-import": {}, // Line 1
-    "postcss-nested": {}, // Line 2
-    tailwindcss: {},
-    'tailwindcss/nesting': 'postcss-nested', // or 'postcss-nesting'
-    autoprefixer: {},
+    '@tailwindcss/postcss': {},
   },
 }
