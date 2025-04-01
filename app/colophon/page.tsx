d2040d10054db4f9e6b9a285b1529bd2ce4f65cb
Ryan Fitton <ryan@ryanfitton.co.uk>
2025-04-01 15:20:45 +0100

    Upload site domain changes

--- app/colophon/page.tsx
+++ app/colophon/page.tsx
@@ -1,12 +1,12 @@
-import siteMetadata from './data/siteMetadata'
+import siteMetadata from '@/data/siteMetadata'
 import Link from '@/components/Link'
 import { genPageMetadata } from 'app/seo'
 
 export const metadata = genPageMetadata({ title: 'Colophon' })
 
 export default function Colophon() {
-  const urlBase = siteMetadata.siteUrl.match(/^https?:\/\/([^/]+)/)[1];
-  const urlBaseHyphen = urlBase.replace(/\./g, "-").replace(/:/g, "-");
+  const siteDomain = siteMetadata.siteUrl.match(/^https?:\/\/([^/]+)/)[1];
+  const siteDomainHyphen = siteDomain.replace(/\./g, "-").replace(/:/g, "-");
 
   return (
     <>
@@ -57,15 +57,15 @@ export default function Colophon() {
               <li>
                 <p>
                     Google PageSpeed scores:{' '}
-                  <Link href={`https://pagespeed.web.dev/analysis/${urlBase}/tdo0bb78bj?form_factor=desktop`}>Desktop (xx/100)</Link>{' '}
+                  <Link href={`https://pagespeed.web.dev/analysis/${siteDomain}/tdo0bb78bj?form_factor=desktop`}>Desktop (xx/100)</Link>{' '}
                   and{' '}
-                  <Link href={`https://pagespeed.web.dev/analysis/${urlBase}/tdo0bb78bj?form_factor=mobile`}>Mobile (xx/100)</Link>
+                  <Link href={`https://pagespeed.web.dev/analysis/${siteDomain}/tdo0bb78bj?form_factor=mobile`}>Mobile (xx/100)</Link>
                 </p>
               </li>
               <li>
                 <p>
                     Mozilla Observatory score:{' '}
-                  <Link href={`https://developer.mozilla.org/en-US/observatory/analyze?host=${urlBaseHyphen}`}>Report (xx/100)</Link>.
+                  <Link href={`https://developer.mozilla.org/en-US/observatory/analyze?host=${siteDomainHyphen}`}>Report (xx/100)</Link>.
                 </p>
               </li>
             </ul>
